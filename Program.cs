542ced495cf19d627deaa0b2406e52f88ccca74c
unknown <nksayyad2900.com>
2020-12-03 23:31:10 +0530

    [NIJAM] Add:Repeting Till The player win-100 position

--- Program.cs
+++ Program.cs
@@ -20,22 +20,24 @@ namespace SnakeAndLadder
         {
             Console.WriteLine("***Welcome To Snake and Ladder***");
             int playerPosition = 0;
-
-            int playerDice = rollDice();
-            int option = checkOption();
-            switch (option)
+            while (playerPosition != 100)
             {
-                case 1:
-                    Console.WriteLine("Player Got Ladder.");             
-                    break;
-                case 2:
-                    Console.WriteLine("Player got Snake.");
-                    break;
-                default:
-                    Console.WriteLine("Player not playing.");
-                    break;
-
-
+                int playerDice = rollDice();
+                int option = checkOption();
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Player Got Ladder.");
+                        playerPosition += playerDice;
+                        break;
+                    case 2:
+                        Console.WriteLine("Player got Snake.");
+                        playerPosition -= playerDice;
+                        break;
+                    default:
+                        Console.WriteLine("Player not playing.");
+                        break;
+                }
             }
         }
     }
