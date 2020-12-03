1d32e2b59abd523fdce6aaefccb0c72a602c3432
unknown <nksayyad2900.com>
2020-12-03 23:25:27 +0530

    [NIJAM] Add:Player Checking Option.

--- Program.cs
+++ Program.cs
@@ -4,17 +4,39 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        public static int rollDice()
+        {
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            return dice;
+        }
+        public static int checkOption()
+        {
+            Random random = new Random();
+            int option = random.Next(1,4);
+            return option;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("***Welcome To Snake and Ladder***");
             int playerPosition = 0;
+
             int playerDice = rollDice();
-        }
-        public static int rollDice()
-        {
-            Random random = new Random();
-            int dice = random.Next(1,7);
-            return dice;
+            int option = checkOption();
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Player Got Ladder.");             
+                    break;
+                case 2:
+                    Console.WriteLine("Player got Snake.");
+                    break;
+                default:
+                    Console.WriteLine("Player not playing.");
+                    break;
+
+
+            }
         }
     }
 }
