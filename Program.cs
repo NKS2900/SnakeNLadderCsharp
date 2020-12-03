b611799b80bf75077b2f6a0eada2704bf2d52815
unknown <nksayyad2900.com>
2020-12-03 23:06:06 +0530

    [NIJAM] Add:Rolling Dice

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ namespace SnakeAndLadder
         {
             Console.WriteLine("***Welcome To Snake and Ladder***");
             int playerPosition = 0;
+            int playerDice = rollDice();
+        }
+        public static int rollDice()
+        {
+            Random random = new Random();
+            int dice = random.Next(1,7);
+            return dice;
         }
     }
 }
