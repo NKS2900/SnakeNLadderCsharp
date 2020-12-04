cd914af05e3b8ece7f46e43baf04c842e32dfe2b
unknown <nksayyad2900.com>
2020-12-04 08:43:50 +0530

    [NIJAM] Add:TwoPlayer Playing And If player got ladder Plya aagain

--- Program.cs
+++ Program.cs
@@ -19,45 +19,70 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("***Welcome To Snake and Ladder***");
-            int playerPosition = 0;
-            int winPosition = 100;
-            int countRound = 0;
-            while (playerPosition != 100)
+            int playerOnePosition = 0, playerTwoPosition = 0, winPosition = 100, countRound = 0;
+
+            while (playerOnePosition != 100 && playerTwoPosition != 100)
             {
-                int playerDice = rollDice();
+                int playerOneDice = rollDice();
+                int playerTwoDice = rollDice();
                 int option = checkOption();
-                countRound++;              
+                countRound++;
                 switch (option)
                 {
                     case 1:
                         Console.WriteLine("Player Got Ladder.");
-                        playerPosition += playerDice;
-                        if (playerPosition > winPosition)
+                        playerOnePosition += playerOneDice;
+                        playerTwoPosition += playerTwoDice;
+                        int OneChance = rollDice();
+                        playerOnePosition += playerOneDice;//If got ladder Rolling again
+                        int TwoChance = rollDice();
+                        playerTwoPosition += playerTwoDice;//If got ladder Rolling again
+                        if (playerOnePosition > winPosition)
                         {
-                            playerPosition -= playerDice;
+                            playerOnePosition -= playerOneDice;                          
+                        }
+                        else if (playerTwoPosition > winPosition)
+                        {
+                            playerTwoPosition -= playerTwoDice;
                         }
                         break;
                     case 2:
                         Console.WriteLine("Player got Snake.");
-                        if ((playerPosition - playerDice) < 0)
+                        if ((playerOnePosition - playerOneDice) < 0)
                         {
-                            playerPosition = 0;
+                            playerOnePosition = 0;
+
+                        }
+                        else if ((playerTwoPosition - playerTwoDice) < 0)
+                        {
+                            playerTwoPosition = 0;
                         }
                         else
                         {
-                            playerPosition -= playerDice;
+                            playerOnePosition -= playerOneDice;
+                            playerTwoPosition -= playerTwoDice;
                         }
                         break;
                     default:
                         Console.WriteLine("Player not playing.");
                         break;
                 }
-                Console.WriteLine($"Roll_Dice_Count:{ countRound } :Player_Position: {playerPosition}");
-                if (playerPosition == winPosition)
+                Console.WriteLine($"Roll_Dice_Count:{ countRound } :Player_One_Position: {playerOnePosition}");
+                Console.WriteLine($"Roll_Dice_Count:{ countRound } :Player_Two_Position: {playerTwoPosition}");
+                if (playerOnePosition == winPosition || playerTwoPosition == winPosition)
                 {
-                    Console.WriteLine("Player Won The SnkeNLadder***");
-                    Console.WriteLine("Roll_Dice_Count: " + countRound);
-                    break;
+                    if (playerOnePosition == winPosition)
+                    {
+
+                        Console.WriteLine("----------Player_One_Won----------");
+                        Console.WriteLine("Number of times dice was played : {0}", countRound);                        
+                    }
+                    else if (playerTwoPosition == winPosition)
+                    {
+
+                        Console.WriteLine("----------Player_Two_Won----------");
+                        Console.WriteLine("Number of times dice was played : {0}", countRound);                       
+                    }
                 }
             }
         }
